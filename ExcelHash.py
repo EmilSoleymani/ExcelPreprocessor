7faa9006c2b5ca9d7b0093e6e69fc72090fbe6a5
Emil Soleymani <soleymae@mcmaster.ca>
2022-09-28 15:17:21 -0400

    Added comments.

--- ExcelHash.py
+++ ExcelHash.py
@@ -13,6 +13,8 @@ def getHash(a: str) -> str:
     """
     Returns hash of input string `a`. Uses `sha256` hashing algorithm.
     """
+
+    # Make sure type of data is str
     if not type(a) == str:
         a = str(a)
     return hashlib.sha256(a.encode('utf-8')).hexdigest()
@@ -23,30 +25,44 @@ if __name__ == '__main__':
     hashCols = []
     delCols = []
 
+    # Read columns to hash
     with open('hashColumns.txt', 'r') as f:
         hashCols = f.readlines()
     
+    # Read columns to delete
     with open('deleteColumns.txt', 'r') as f:
         delCols = f.readlines()
 
+    # Names of all valid columns
     colNames = data.columns.to_list()
 
+    # For all specified columns to hash
     for col in hashCols:
+        # Remove newline at end of line (or possible empty spaces)
         colName = col.strip()
 
+        # If column doesn't exist
         if not colName in colNames:
+            # Print warning and go to next column to hash
             print("Warning: Cannot hash non-existant column \"", colName, "\" from data!", sep="")
             continue
+        
+        # Hash all values in a column
+        data[colName] = data[colName].apply(getHash)
 
-        data[col.strip()] = data[col.strip()].apply(getHash)
-
+    # For all specified columns to delete
     for col in delCols:
+        # Remove newline newline at end of line (or possible empty spaces)
         colName = col.strip()
 
+        # If column doesn't exist
         if not colName in colNames:
+            # Print warning and go to next column to delete
             print("Warning: Cannot delete non-existant column \"", colName, "\" from data!", sep="")
             continue
-
+        
+        # Drop column
         data = data.drop(columns=[colName])
 
+    # Write to file
     data.to_excel('./test.xlsx')
